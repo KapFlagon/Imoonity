c7b31836cc43c227ceb248f62dd99458f78370f1
Jack Grimes <flangojack@hotmail.com>
2020-11-11 21:07:54 +0000

    Basic dash implementation.

--- src/scenes/game_objects/scripts/Player.gd
+++ src/scenes/game_objects/scripts/Player.gd
@@ -7,6 +7,7 @@ export var air_friction = 0.02
 export var base_movement_speed = 300
 export var max_movement_speed = 100
 export var jump_force = 150
+var dash_used = false
 var velocity = Vector2(0, 0)
 
 
@@ -25,6 +26,8 @@ func _physics_process(delta: float) -> void:
 func update_player_velocity(delta: float) -> void:
 	velocity.x = update_player_horizontal_velocity()
 	velocity.y = update_player_vertical_velocity(delta, velocity.y) 
+	if Input.is_action_just_pressed("power_1") and !dash_used:
+		velocity.x = launch_dash(velocity.x)
 
 
 func update_player_horizontal_velocity() -> float:
@@ -69,3 +72,15 @@ func update_player_vertical_velocity(delta: float, current_vertical_velocity: fl
 		if Input.is_action_just_released("move_jump") and current_vertical_velocity < (-jump_force/2):
 			vertical_velocity = current_vertical_velocity + (jump_force/2)
 	return vertical_velocity
+
+
+func launch_dash(current_horizontal_velocity: float) -> float: 
+	var dash_force = 2000
+	var input_dir = get_horizontal_input()
+	var output_horizontal_velocity
+	if input_dir >= 0:
+		output_horizontal_velocity = current_horizontal_velocity + dash_force
+	elif input_dir < 0: 
+		output_horizontal_velocity = current_horizontal_velocity - dash_force
+	
+	return output_horizontal_velocity
