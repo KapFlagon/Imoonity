9d4d9c49ba6938d204e355aef4f887f8d37ae490
Ciaran Doyle <irishrocker182@gmail.com>
2020-11-13 15:29:04 -0800

    ParticleEffects
    Playing with some particle Effects to show phase ability is activated

--- src/scenes/game_objects/phaseAbility/phaseAbilityManager.gd
+++ src/scenes/game_objects/phaseAbility/phaseAbilityManager.gd
@@ -1,17 +1,28 @@
 extends Node
 
 var playerScene 
+var particles 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	playerScene = get_parent()
+	particles =  preload("res://src/scenes/game_objects/phaseAbility/PhasingParticle.tscn").instance()
 
 
 func checkActionButtonPressed():
 	if Input.is_action_just_pressed("action_phase"):
 		if(playerScene.get_collision_mask_bit(2) ):
 			playerScene.set_collision_mask_bit(2,false)
+			activateParticles()
 		else:
 			playerScene.set_collision_mask_bit(2,true)
+			deactivateParticles()
+			
+func activateParticles():
+	playerScene.add_child(particles) # They have a very high Z position, so being buried would not be a problem
+	particles.set_emitting(true)
+
+func deactivateParticles():
+	particles.set_emitting(false)
 
