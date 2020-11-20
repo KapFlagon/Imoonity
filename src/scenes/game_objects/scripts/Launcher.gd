7a10b0fd91e7806b7c36635824a486cf47581a16
Jack Grimes <flangojack@hotmail.com>
2020-11-20 12:20:38 +0000

    Changed sprites for launcher. Added animations. Added function to calculate animation speed. Added export var for launcher frequency.

--- src/scenes/game_objects/scripts/Launcher.gd
+++ src/scenes/game_objects/scripts/Launcher.gd
@@ -1,25 +1,35 @@
 extends StaticBody2D
 
+# Exported variables
 export (PackedScene) var projectile
-var projectileInstance
+export var firing_interval: int = 1		# A variable to define how long the timer should last, in seconds.
 
-var timer = 0
-# A variable to define how long the timer should last, in seconds.
-var timer_wait_time = 2
+
+# Non exported variables
+var projectile_instance
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-	
-func _process(delta):
-	timer += delta
-	if (timer >= timer_wait_time):
-		timer -= timer_wait_time
-		fireNewProjectile()
-	   
+	$Timer.wait_time = firing_interval
 
 
 func fireNewProjectile():
-	projectileInstance = projectile.instance()
-	add_child(projectileInstance)
+	projectile_instance = projectile.instance()
+	add_child(projectile_instance)
+
+
+func _on_Timer_timeout() -> void:
+	fireNewProjectile()
+	if !$AnimationPlayer.is_playing():
+		var animation_time_scaling = calculate_animation_speed_scale(firing_interval)
+		$AnimationPlayer.set_speed_scale(animation_time_scaling)
+		$AnimationPlayer.play("firing")
+
+
+# Calculates playback speed for the firing animation based on the firing interval value
+func calculate_animation_speed_scale(firing_interval: int) -> float: 
+	var output_scale: float = 1
+	if firing_interval != 1:
+		output_scale = 1.0 / (firing_interval / output_scale)
+	return output_scale
