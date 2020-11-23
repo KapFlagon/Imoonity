ac15678e201ea637cf113324c8b3d00fb9fb85bc
Jack Grimes <flangojack@hotmail.com>
2020-11-23 22:54:59 +0000

    Updating the level exit node.

--- src/scenes/game_objects/scripts/LevelExit.gd
+++ src/scenes/game_objects/scripts/LevelExit.gd
@@ -1,16 +1,26 @@
 extends Node2D
 
 
+# Custom Signals
+signal level_ended
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	connect("level_ended", get_parent().get_node("Transitions"), "play_fade_out")
 	pass # Replace with function body.
 
 
-
-
-func _on_Area2D_area_exited(area: Area2D) -> void:
-	print("area parent:" + str(area.get_parent().get_name()))
+func _on_AnimationSensor_area_entered(area: Area2D) -> void:
 	if area.get_parent().name == "Player":
 		$LevelExitSprite.play("open")
-		print("here")
-		pass # Replace with function body.
+
+
+func _on_AnimationSensor_area_exited(area: Area2D) -> void:
+	if area.get_parent().name == "Player":
+		$LevelExitSprite.play("close")
+
+
+func _on_LevelExitSensor_area_entered(area: Area2D) -> void:
+	if area.get_parent().name == "Player":
+		emit_signal("level_ended")
