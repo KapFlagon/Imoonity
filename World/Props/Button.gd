6a0781b814d8a3bc56a51c749397a7478b183c43
Paul Donovan <pauldonovandonegal@gmail.com>
2020-11-24 16:33:47 +0000

    Fixed button layer and mask

--- World/Props/Button.gd
+++ World/Props/Button.gd
@@ -11,7 +11,8 @@ signal unpressed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	animated_sprite.play("Off")
+	#animated_sprite.play("Off")
+	#emit_signal("unpressed")
 	connect("body_entered", self, "_on_body_entered")
 	connect("body_exited", self, "_on_body_exited")
 	
