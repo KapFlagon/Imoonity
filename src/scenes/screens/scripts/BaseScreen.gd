4310fc568dccacdeebc40293b1e7c49713d7b487
Jack Grimes <flangojack@hotmail.com>
2020-11-26 01:59:19 +0000

    Changing the Screen Manager update logic in a minor way.

--- src/scenes/screens/scripts/BaseScreen.gd
+++ src/scenes/screens/scripts/BaseScreen.gd
@@ -3,5 +3,4 @@ extends Control
 class_name BaseScreen
 
 func poll_screen_manager(this_screen_value: int) -> void:
-	if ScreenManager.get_current_screen() == -1:
-		ScreenManager.set_current_screen(this_screen_value)
+	ScreenManager.set_current_screen(this_screen_value)
