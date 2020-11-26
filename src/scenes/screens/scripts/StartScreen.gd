510f2ba244e37718d96a6f96b3521062943a67b7
Jack Grimes <flangojack@hotmail.com>
2020-11-26 00:32:48 +0000

    Adding some script logic for the credits screen to redirect to the main menu.

--- src/scenes/screens/scripts/StartScreen.gd
+++ src/scenes/screens/scripts/StartScreen.gd
@@ -1,8 +1,9 @@
-extends Control
+extends BaseScreen
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	poll_screen_manager(Enums.SCREENS.MAIN_MENU)
 	pass # Replace with function body.
 
 
@@ -11,4 +12,9 @@ func _on_GithubLinkButton_button_up() -> int:
 
 
 func _on_PlayButton_button_up() -> void:
-	get_tree().change_scene("res://src/scenes/levels/prototypes/TemplateLevel.tscn")
+	ScreenManager.next_screen(Enums.SCREENS.INTRO)
+
+
+func _on_Credits_button_up() -> void:
+	ScreenManager.next_screen(Enums.SCREENS.CREDITS)
+
