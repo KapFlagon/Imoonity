896ab41e43070e830462ef47c9e9cf55a32cf01b
Jack Grimes <flangojack@hotmail.com>
2020-11-27 18:03:23 +0000

    Updating the dash manager to emit a signal at regular intervals when it is running.

--- src/scenes/game_objects/PuckAbility/scripts/Puck_Dash.gd
+++ src/scenes/game_objects/PuckAbility/scripts/Puck_Dash.gd
@@ -1,8 +1,11 @@
 extends Node
 
 
+signal puck_timer_elapsing(percentage_elapsed)
+
+
 export (int) var dash_force = 2000
-export (int) var cooldown = 1
+export (float) var cooldown_time_seconds = 1
 
 
 var player_scene = null
@@ -14,7 +17,11 @@ var action_button: String setget set_action_button, get_action_button
 func _ready() -> void:
 	action_button = "power_1"
 	player_scene = get_parent()
-	$Timer.set_wait_time(cooldown)
+	$Timer.set_wait_time(cooldown_time_seconds)
+
+
+func _process(delta) -> void:
+	broadcast_elapsed_time_percentage()
 
 
 func set_ability_active(ability_state: bool) -> void:
@@ -53,5 +60,16 @@ func launch_dash(current_velocity: Vector2) -> void:
 
 
 func _on_Timer_timeout() -> void:
+	$Timer.stop()
 	dash_available = true
+	emit_signal("puck_timer_elapsing", 100)
+
+
+func broadcast_elapsed_time_percentage() -> void: 
+	if !$Timer.is_stopped():
+		var cooldown_time_left: float = $Timer.get_time_left()
+		var cooldown_elapsed_time: float = cooldown_time_seconds - cooldown_time_left
+		var elapsed_percentage: float = cooldown_elapsed_time / cooldown_time_seconds
+		elapsed_percentage = elapsed_percentage * 100
+		emit_signal("puck_timer_elapsing", elapsed_percentage)
 
