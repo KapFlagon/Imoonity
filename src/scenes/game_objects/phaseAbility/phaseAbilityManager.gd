ecf0ed228b4da6771b249f8baf7db26541c3bf02
Jack Grimes <flangojack@hotmail.com>
2020-11-30 21:45:54 +0000

    Added a new icon to HUD for indicating whether or not Io phasing ability is active.

--- src/scenes/game_objects/phaseAbility/phaseAbilityManager.gd
+++ src/scenes/game_objects/phaseAbility/phaseAbilityManager.gd
@@ -1,6 +1,9 @@
 extends Node
 
+signal power_triggered(power_active)
+
 var playerScene 
+var power_active: bool = false setget set_power_active, is_power_active
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,7 +16,17 @@ func checkActionButtonPressed():
 		if(playerScene.get_collision_mask_bit(2)):
 			playerScene.set_collision_mask_bit(2,false)
 			playerScene.find_node("Sprite").self_modulate = Color(0.44, 0.5, 0.56, 0.5 )
+			set_power_active(true)
 		else:
 			playerScene.set_collision_mask_bit(2,true)
 			playerScene.find_node("Sprite").self_modulate = Color(1,1,1,1)
+			set_power_active(false)
 
+
+func set_power_active(new_value: bool) -> void:
+	power_active = new_value
+	emit_signal("power_triggered", new_value)
+
+
+func is_power_active() -> bool: 
+	return power_active
