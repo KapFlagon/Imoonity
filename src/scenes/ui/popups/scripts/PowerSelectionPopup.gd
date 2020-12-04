2cf65ac9039b4ccdfea84238bdae2b9ff910042f
Jack Grimes <flangojack@hotmail.com>
2020-12-04 10:02:13 +0000

    Updated the Powerup selection pop-up so that it will dynamically display powers depending on user selections, etc.

--- src/scenes/ui/popups/scripts/PowerSelectionPopup.gd
+++ src/scenes/ui/popups/scripts/PowerSelectionPopup.gd
@@ -3,12 +3,20 @@ extends PopupDialog
 
 export var max_active_powers = 2
 var active_power_count = 0
+var show_puck_dash: bool setget set_show_puck_dash
+var show_io_phase: bool setget set_show_io_phase
+var show_phobos_strength: bool setget set_show_phobos_strength
+var show_titan_plat: bool setget set_show_titan_plat
 
 
 # Child node variable assignments
+onready var puck_VBox = get_node("PowersHBox/PuckDashVBox")
 onready var puck_checkbox = get_node("PowersHBox/PuckDashVBox/CheckBox")
+onready var io_VBox = get_node("PowersHBox/IOPhaseVBox")
 onready var io_checkbox = get_node("PowersHBox/IOPhaseVBox/CheckBox")
+onready var phobos_VBox = get_node("PowersHBox/PhobosStrengthVBox")
 onready var phobos_checkbox = get_node("PowersHBox/PhobosStrengthVBox/CheckBox")
+onready var titan_VBox = get_node("PowersHBox/TitanPlatformVBox")
 onready var titan_checkbox = get_node("PowersHBox/TitanPlatformVBox/CheckBox")
 
 
@@ -37,10 +45,15 @@ func _on_CancelBtn_button_up() -> void:
 
 
 func update_display_content() -> void:
+	update_puck_display()
+	update_io_display()
+	update_phobos_display()
+	update_titan_display()
+
+
+func update_puck_display() -> void:
 	update_puck_values()
-	update_io_values()
-	update_phobos_values()
-	update_titan_values()
+	update_puck_visibility()
 
 
 func update_puck_values() -> void: 
@@ -50,11 +63,35 @@ func update_puck_values() -> void:
 		puck_checkbox.set_pressed(false)
 
 
+func update_puck_visibility() -> void:
+	if show_puck_dash:
+		puck_VBox.show()
+	else: 
+		puck_VBox.hide()
+
+
+func update_io_display():
+	update_io_values()
+	update_io_visibility()
+
+
 func update_io_values() -> void: 
 	if PowerManager.is_io_equipped():
 		io_checkbox.set_pressed(true)
 	elif !PowerManager.is_io_equipped():
 		io_checkbox.set_pressed(false)
+
+
+func update_io_visibility() -> void:
+	if show_io_phase:
+		io_VBox.show()
+	else:
+		io_VBox.hide()
+
+
+func update_phobos_display() -> void:
+	update_phobos_values()
+	update_phobos_visibility()
 
 
 func update_phobos_values() -> void: 
@@ -64,16 +101,30 @@ func update_phobos_values() -> void:
 		phobos_checkbox.set_pressed(false)
 
 
+func update_phobos_visibility() -> void:
+	if show_phobos_strength:
+		phobos_VBox.show()
+	else:
+		phobos_VBox.hide()
+
+
+func update_titan_display() -> void:
+	update_titan_values()
+	update_titan_visibility()
+
+
 func update_titan_values() -> void: 
 	if PowerManager.is_titan_equipped():
 		titan_checkbox.set_pressed(true)
 	elif !PowerManager.is_titan_equipped():
-		titan_checkbox.set_pressed(true)
+		titan_checkbox.set_pressed(false)
 
 
-func lock_checkboxes() -> void:
-	
-	pass
+func update_titan_visibility() -> void:
+	if show_titan_plat:
+		titan_VBox.show()
+	else:
+		titan_VBox.hide()
 
 
 func _on_CheckBox_toggled(button_pressed: bool) -> void:
@@ -101,4 +152,20 @@ func ready_interface(camera_center: Vector2) -> void:
 	get_tree().paused = true
 	update_display_content()
 	update_display_location(camera_center)
+
+
+func set_show_puck_dash(new_value: bool) -> void: 
+	show_puck_dash = new_value
+
+
+func set_show_io_phase(new_value: bool) -> void: 
+	show_io_phase = new_value
+
+
+func set_show_phobos_strength(new_value: bool) -> void: 
+	show_phobos_strength = new_value
+
+
+func set_show_titan_plat(new_value: bool) -> void: 
+	show_titan_plat = new_value
 
