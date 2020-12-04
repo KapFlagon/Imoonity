af9e81411750681a9a11a801d7bb2ea3c27883c8
Jack Grimes <flangojack@hotmail.com>
2020-12-04 10:06:47 +0000

    Push exported variables down from the checkpoint into the pop-up.

--- src/scenes/game_objects/scripts/Checkpoint.gd
+++ src/scenes/game_objects/scripts/Checkpoint.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
 
+# Custom Signals
+signal powers_updated
+
+
 # Export variables
 export var puck_dash_available: bool
 export var phobos_strength_available: bool
@@ -14,21 +18,20 @@ var player_ref
 
 
 func _ready() -> void:
-	pass # Replace with function body.
+	connect("powers_updated", get_parent().get_node("Player/Camera2D/PlayerHud"), "update_all_hud_icons")
+	$PowerSelectionPopup.set_show_puck_dash(puck_dash_available)
+	$PowerSelectionPopup.set_show_io_phase(io_phasing_available)
+	$PowerSelectionPopup.set_show_phobos_strength(phobos_strength_available)
+	$PowerSelectionPopup.set_show_titan_plat(titan_platforms_available)
 
 
 func _process(delta: float) -> void:
 	check_for_input()
 
 
-func _on_CheckPoint_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	pass
-
-
 func check_for_input() -> void:
 	if player_overlap and Input.is_action_just_released("checkpoint_action"):
 		var camera_center = get_parent().get_node("Player").get_node("Camera2D").get_camera_screen_center()
-		print("camera center: " + str(camera_center))
 		$PowerSelectionPopup.ready_interface(camera_center)
 
 
@@ -41,3 +44,6 @@ func _on_CheckPoint_body_exited(body: Node) -> void:
 	if body.get_name() == "Player":
 		player_overlap = false
 
+
+func _on_PowerSelectionPopup_popup_hide() -> void:
+	emit_signal("powers_updated")
