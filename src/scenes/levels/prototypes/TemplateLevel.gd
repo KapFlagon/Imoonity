491492baefaf2b131ddb23c3af67eec394d152f8
Jack Grimes <flangojack@hotmail.com>
2020-12-04 17:56:12 +0000

    Updated the Template Level script file so that it uses the base level class.

--- src/scenes/levels/prototypes/TemplateLevel.gd
+++ src/scenes/levels/prototypes/TemplateLevel.gd
@@ -1,26 +1,8 @@
-extends Node2D
-
-
-var player_spawn_coordinates: Vector2 = Vector2.ZERO setget set_player_spawn_coordinates, get_player_spawn_coordinates
+extends BaseLevel
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	push_data_to_screen_manager()
-	set_player_spawn_coordinates(ScreenManager.get_player_start_coordinates())
-	if get_player_spawn_coordinates() != Vector2.ZERO:
-		$Player.set_position(player_spawn_coordinates)
+	set_level_screen_value(Enums.SCREENS.TEMPLATE)
 	#MusicManager.tutorialLevelMusic()
 
-
-func set_player_spawn_coordinates(new_value: Vector2) -> void:
-	player_spawn_coordinates = new_value
-
-
-func get_player_spawn_coordinates() -> Vector2:
-	return player_spawn_coordinates
-
-
-func push_data_to_screen_manager() -> void:
-	if ScreenManager.get_current_screen() == -1:
-		ScreenManager.set_current_screen(Enums.SCREENS.TEMPLATE)
