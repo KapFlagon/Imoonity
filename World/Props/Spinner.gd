c1fa96248b2d1f66b8903f48649efc6470f6b029
Jack Grimes <flangojack@hotmail.com>
2020-12-11 14:00:20 +0000

    Updating the spinner to use the base SwitchableObstacle class.

--- World/Props/Spinner.gd
+++ World/Props/Spinner.gd
@@ -1,8 +1,32 @@
-extends StaticBody2D
+extends SwitchableObstacle
 
 
-export var speed: int = 1	  # A variable to define how fast the spinner spins
+export var _speed: int = 1 setget set_speed, get_speed
+
+var _default_speed: int 
+
+
+func _ready():
+	_default_speed = _speed
 
 
 func _physics_process(delta):
-	rotation_degrees = rotation_degrees + speed
+	if is_obstacle_active():
+		rotation_degrees = rotation_degrees + _speed
+
+
+func set_speed(new_value: int) -> void: 
+	_speed = new_value
+
+
+func get_speed() -> int:
+	return _speed
+
+
+func reset_speed() -> void:
+	set_speed(_default_speed)
+
+
+func flip_active_state() -> void:
+	.flip_active_state()
+
