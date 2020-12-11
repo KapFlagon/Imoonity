bbef92a790ea053f7b709251d8eea3717ae4e5e5
Jack Grimes <flangojack@hotmail.com>
2020-12-11 10:44:37 +0000

    Refactored some Spike code. Now functions with the switachable functionality from the base class.

--- src/scenes/game_objects/obstacles/scripts/Spike.gd
+++ src/scenes/game_objects/obstacles/scripts/Spike.gd
@@ -1,11 +1,46 @@
-extends Area2D
+extends SwitchableObstacle
 
+
+onready var _visibility_enabler = get_node("VisibilityEnabler2D")
+onready var _tip_sprite = get_node("Tip")
+onready var _animation_player = get_node("AnimationPlayer")
+onready var _shape = get_node("Area2D/CollisionShape2D")
+onready var _audio = get_node("SpikeAudio")
+
+
+func _ready():
+	_connect_visibility_enabler_signals()
 
 
 func _on_VisibilityEnabler2D_screen_entered() -> void:
-	$AnimationPlayer.play("flash")
-	$SpikeAudio.play()
+	if is_obstacle_active():
+		_animation_player.play("flash")
+		_audio.play()
 
 
 func _on_VisibilityEnabler2D_screen_exited() -> void:
-	$AnimationPlayer.stop()
+	_animation_player.stop()
+
+
+func _connect_visibility_enabler_signals() -> void:
+	_visibility_enabler.connect("screen_entered", self, "_on_VisibilityEnabler2D_screen_entered")
+	_visibility_enabler.connect("screen_exited", self, "_on_VisibilityEnabler2D_screen_exited")
+
+
+func _disconnect_visibility_enabler_signals() -> void:
+	_visibility_enabler.disconnect("screen_entered", self, "_on_VisibilityEnabler2D_screen_entered")
+	_visibility_enabler.disconnect("screen_exited", self, "_on_VisibilityEnabler2D_screen_exited")
+
+
+func flip_active_state() -> void:
+	if is_obstacle_active():
+		_disconnect_visibility_enabler_signals()
+		_tip_sprite.hide()
+		_shape.set_deferred("disabled", true)
+		_animation_player.stop()
+	else:
+		_tip_sprite.show()
+		_shape.set_deferred("disabled", false)
+		_animation_player.play("flash")
+		_connect_visibility_enabler_signals()
+	.flip_active_state()
