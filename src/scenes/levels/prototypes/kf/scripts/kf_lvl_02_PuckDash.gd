f7dbca7d6e8dc74834b7797d335de0e9a528c164
Jack Grimes <flangojack@hotmail.com>
2020-12-14 18:15:54 +0000

    Updates and finalizing KF Puck intro level.

--- src/scenes/levels/prototypes/kf/scripts/kf_lvl_02_PuckDash.gd
+++ src/scenes/levels/prototypes/kf/scripts/kf_lvl_02_PuckDash.gd
@@ -1,1 +1,17 @@
 extends BaseLevel
+
+
+func _on_Button_button_just_pressed():
+	flip_all_spikes()
+	get_node("TimerContainer/SpikeTimer").start()
+
+
+func _on_SpikeTimer_timeout():
+	flip_all_spikes()
+	get_node("TimerContainer/SpikeTimer").stop()
+
+
+func flip_all_spikes() -> void:
+	for each_spike in get_node("SpikeContainer").get_children():
+		each_spike.flip_active_state()
+
