998f55e2b4cc58a54f8509b1e8bb5f149d52f264
Jack Grimes <flangojack@hotmail.com>
2020-12-15 11:39:10 +0000

    Changed structure of the pop-up code so that it auto-centers instead of displaying off center.

--- src/scenes/ui/popups/scripts/pauseMenu.gd
+++ src/scenes/ui/popups/scripts/pauseMenu.gd
@@ -1,32 +1,23 @@
 extends Popup
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 func update_display_location(camera_center: Vector2) -> void:
 	var width = rect_size.x * rect_scale.x
 	var height = rect_size.y * rect_scale.y
 	var x = camera_center.x - (width / 2)
 	var y = camera_center.y - (height / 2) 
 	var new_position = Vector2(x, y)
+	popup()
 	rect_position = new_position
-	
+
+
 func pause_game() -> void:
 	get_tree().paused = true
-	
+
+
 func un_pause_game() -> void:
 	get_tree().paused = false
 	hide()
-	
-	
 
 
 func _on_resetLevel_button_up():
@@ -36,7 +27,8 @@ func _on_resetLevel_button_up():
 func _on_mainMenu_button_up():
 	ScreenManager.next_screen(Enums.SCREENS.MAIN_MENU)
 	MusicManager.music_player.stop()
-	
+
+
 func _on_cancel_button_up():
 	un_pause_game()
 
@@ -48,4 +40,6 @@ func _on_pauseMenu_popup_hide():
 	un_pause_game()
 
 
-
+func show_interface(camera_center: Vector2) -> void:
+	pause_game()
+	update_display_location(camera_center)
