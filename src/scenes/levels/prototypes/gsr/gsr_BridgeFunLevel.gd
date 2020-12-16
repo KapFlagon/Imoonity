aa31c7a91123a41898821c459122044c72db828f
Paul Donovan <pauldonovandonegal@gmail.com>
2020-12-16 00:48:56 +0000

    Updated all gsr levels to abide by new button logic

--- src/scenes/levels/prototypes/gsr/gsr_BridgeFunLevel.gd
+++ src/scenes/levels/prototypes/gsr/gsr_BridgeFunLevel.gd
@@ -2,15 +2,12 @@ extends BaseLevel
 
 
 func _ready():
-	MusicManager.play_song("res://assets/music/Godot_Slurp_Master1.ogg")
-
-func _on_BridgeButton_pressed():
-	var bridge = get_node("BridgeContainer/Bridge")
-	if !bridge.animPlayer.is_playing():
-		bridge.rotateBridge()
+	pass#MusicManager.play_song("res://assets/music/Godot_Slurp_Master1.ogg")
 
 
-func _on_BridgeButton2_pressed():
-	var bridge = get_node("BridgeContainer/Bridge2")
-	if !bridge.animPlayer.is_playing():
-		bridge.rotateBridge()
+func _on_BridgeButton_button_just_pressed():
+	$BridgeContainer/Bridge.rotateBridge()
+
+
+func _on_BridgeButton2_button_just_pressed():
+	$BridgeContainer/Bridge2.rotateBridge()
--- src/scenes/levels/prototypes/gsr/gsr_Confusing.gd
+++ src/scenes/levels/prototypes/gsr/gsr_Confusing.gd
@@ -4,25 +4,13 @@ func _ready():
 	get_node("Player/Camera2D").zoom.x = 1
 	get_node("Player/Camera2D").zoom.y = 1
 
-func _on_BridgeButton_pressed():
-	var bridge = get_node("BridgeContainer/Bridge")
-	if !bridge.animPlayer.is_playing():
-		bridge.rotateBridge()
-	var bridge2 = get_node("BridgeContainer/Bridge2")
-	if !bridge2.animPlayer.is_playing():
-		bridge2.rotateBridge()
-	var bridge5 = get_node("BridgeContainer/Bridge5")
-	if !bridge5.animPlayer.is_playing():
-		bridge5.rotateBridge()
+func _on_BridgeButton_button_just_pressed():
+	$BridgeContainer/Bridge.rotateBridge()
+	$BridgeContainer/Bridge2.rotateBridge()
+	$BridgeContainer/Bridge5.rotateBridge()
 
 
-func _on_BridgeButton2_pressed():
-	var bridge2 = get_node("BridgeContainer/Bridge2")
-	if !bridge2.animPlayer.is_playing():
-		bridge2.rotateBridge()
-	var bridge3 = get_node("BridgeContainer/Bridge3")
-	if !bridge3.animPlayer.is_playing():
-		bridge3.rotateBridge()
-	var bridge4 = get_node("BridgeContainer/Bridge4")
-	if !bridge4.animPlayer.is_playing():
-		bridge4.rotateBridge()
+func _on_BridgeButton2_button_just_pressed():
+	$BridgeContainer/Bridge2.rotateBridge()
+	$BridgeContainer/Bridge3.rotateBridge()
+	$BridgeContainer/Bridge4.rotateBridge()
