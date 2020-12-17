3ca574c961f544e8d91a7769b200805fa169a6ff
Ciaran <irishrocker182@Gmail.com>
2020-12-17 01:47:37 -0800

    Random Button level
    Added Level with random button to open door

--- src/scenes/levels/prototypes/yummy/Scripts/yjb_lvl_04_backtrack.gd
+++ src/scenes/levels/prototypes/yummy/Scripts/yjb_lvl_04_backtrack.gd
@@ -1,11 +1,13 @@
 extends BaseLevel
 
-
 func _ready():
 	MusicManager.play_song("res://assets/music/Godot_Slurp_Master1.ogg")
+	
+	
 
 
 func _on_Button_button_pressed():
 	var bridge = get_node("BridgeContainer/Bridge_open_exit")
 	if not bridge.is_bridge_open():
 		bridge.rotateBridge()
+
---
+++ src/scenes/levels/prototypes/yummy/Scripts/yjb_lvl_05_random_button.gd
@@ -0,0 +1,57 @@
+extends BaseLevel
+
+var chosenButton
+
+
+func _ready():
+	MusicManager.play_song("res://assets/music/Godot_Slurp_Master1.ogg")
+	var listOfButtons = []
+	listOfButtons.push_back(get_node("ButtonContainer/bot_left_button")) 
+	listOfButtons.push_back(get_node("ButtonContainer/top_left_button"))
+	listOfButtons.push_back(get_node("ButtonContainer/top_right_button"))
+	listOfButtons.push_back(get_node("ButtonContainer/bot_right_button"))
+	listOfButtons.push_back(get_node("ButtonContainer/wall_left_button"))
+	listOfButtons.push_back(get_node("ButtonContainer/wall_right_button"))
+
+	randomize()
+	chosenButton =  listOfButtons[randi() % listOfButtons.size()]
+	print(chosenButton.name)
+
+
+func rotate_bridges():
+	var bridge_right = get_node("BridgeContainer/Bridge_right")
+	var bridge_left = get_node("BridgeContainer/Bridge_left")
+	if not bridge_right.is_bridge_open():
+		bridge_right.rotateBridge()
+	if not bridge_left.is_bridge_open():
+		bridge_left.rotateBridge()
+
+
+func _on_bot_left_button_button_just_pressed():
+	if chosenButton.name == "bot_left_button":
+		rotate_bridges()
+		
+
+func _on_top_left_button_button_just_pressed():
+	if chosenButton.name == "top_left_button":
+		rotate_bridges()
+
+
+func _on_top_right_button_button_just_pressed():
+	if chosenButton.name == "top_right_button":
+		rotate_bridges()
+
+
+func _on_bot_right_button_button_just_pressed():
+	if chosenButton.name == "bot_right_button":
+		rotate_bridges()
+
+
+func _on_wall_right_button_button_just_pressed():
+	if chosenButton.name == "wall_right_button":
+		rotate_bridges()
+
+
+func _on_wall_left_button_button_just_pressed():
+	if chosenButton.name == "wall_left_button":
+		rotate_bridges()
