23a88e3ca2bc27dbedab21dca1f6bfcf2f95ad5d
Paul Donovan <pauldonovandonegal@gmail.com>
2020-12-23 15:35:51 +0000

    Small edits to many levels (e.g. added checkpoints)

--- src/scenes/levels/prototypes/gsr/gsr_Puzzle.gd
+++ src/scenes/levels/prototypes/gsr/gsr_Puzzle.gd
@@ -13,3 +13,6 @@ func _on_Button3_button_just_pressed():
 	$PathObstContainer/PathObstacle2.set_movement_speed(50)
 
 
+func _on_Button4_button_just_pressed():
+	$PathObstContainer/PathObstacle3.set_movement_speed(50)
+
