75a9c9b2c8c7071a3769ae964c85737a7bbc6f67
Jack Grimes <flangojack@hotmail.com>
2020-12-23 22:45:56 +0000

    Setting up the screen manager to deal with level changes etc.

--- src/sceneless_scripts/autoload/ScreenManager.gd
+++ src/sceneless_scripts/autoload/ScreenManager.gd
@@ -65,10 +65,71 @@ func determine_next_screen() -> String:
 	match current_screen:
 		Enums.SCREENS.MAIN_MENU: 
 			new_level_path = "res://src/scenes/screens/IntroCutSceneScreen.tscn"
+			set_current_screen(Enums.SCREENS.INTRO)
 		Enums.SCREENS.INTRO: 
-			new_level_path = "res://src/scenes/levels/prototypes/TemplateLevel.tscn"
-		Enums.SCREENS.TEMPLATE: 
-			new_level_path = "res://src/scenes/levels/prototypes/TemplateLevel.tscn"
+			new_level_path = "res://src/scenes/levels/prototypes/kf/kf_lvl_01.tscn"
+			set_current_screen(Enums.SCREENS.KF_LVL_01)
+		Enums.SCREENS.KF_LVL_01: 
+			new_level_path = "res://src/scenes/levels/prototypes/gsr/gsr_PhobosTutorialLevel.tscn"
+			set_current_screen(Enums.SCREENS.GSR_PHOBOS_TUTORIAL_LEVEL)
+		Enums.SCREENS.GSR_PHOBOS_TUTORIAL_LEVEL:
+			new_level_path = "res://src/scenes/levels/prototypes/yummy/yjb_lvl_io_Tutorial.tscn"
+			set_current_screen(Enums.SCREENS.YJB_LVL_IO_TUTORIAL)
+		Enums.SCREENS.YJB_LVL_IO_TUTORIAL:
+			new_level_path = "res://src/scenes/levels/prototypes/yummy/yjb_lvl_titan_Tutorial.tscn"
+			set_current_screen(Enums.SCREENS.YJB_LVL_TITAN_TUTORIAL)
+		Enums.SCREENS.YJB_LVL_TITAN_TUTORIAL:
+			new_level_path = "res://src/scenes/levels/prototypes/kf/kf_lvl_02_PuckDash.tscn"
+			set_current_screen(Enums.SCREENS.KF_LVL_02_PUCK_DASH)
+		Enums.SCREENS.KF_LVL_02_PUCK_DASH:
+			new_level_path = "res://src/scenes/levels/prototypes/yummy/yjb_lvl_05_random_button.tscn"
+			set_current_screen(Enums.SCREENS.YJB_LVL_05_RANDOM_BUTTON)
+			MusicManager.play_song("res://assets/music/Godot_QuantisedFunk_Master1.ogg")
+		Enums.SCREENS.YJB_LVL_05_RANDOM_BUTTON:
+			new_level_path = "res://src/scenes/levels/prototypes/gsr/gsr_BridgeFunLevel.tscn"
+			set_current_screen(Enums.SCREENS.GSR_BRIDGE_FUN_LEVEL)
+		Enums.SCREENS.GSR_BRIDGE_FUN_LEVEL:
+			new_level_path = "res://src/scenes/levels/prototypes/gsr/gsr_SpinnySpin.tscn"
+			set_current_screen(Enums.SCREENS.GSR_SPINNY_SPIN)
+		Enums.SCREENS.GSR_SPINNY_SPIN:
+			new_level_path = "res://src/scenes/levels/prototypes/yummy/yjb_lvl_04_backtrack.tscn"
+			set_current_screen(Enums.SCREENS.YJB_LVL_04_BACKTRACK)
+		Enums.SCREENS.YJB_LVL_04_BACKTRACK:
+#			new_level_path = "res://src/scenes/levels/prototypes/gsr/gsr_Timer.tscn"
+#			new_level_path = "res://src/scenes/levels/prototypes/yummy/yjb_lvl_03_Falling_IO_focused.tscn"
+			new_level_path = "res://src/scenes/levels/prototypes/kf/kf_lvl_ThePit.tscn"
+			set_current_screen(Enums.SCREENS.KF_LVL_THE_PIT)
+		Enums.SCREENS.KF_LVL_THE_PIT:
+			new_level_path = "res://src/scenes/levels/prototypes/yummy/yjb_lvl_03_Falling_IO_focused.tscn"
+			set_current_screen(Enums.SCREENS.YJB_LVL_03_FALLING_IO_FOCUSED)
+		Enums.SCREENS.YJB_LVL_03_FALLING_IO_FOCUSED:
+			new_level_path = "res://src/scenes/levels/prototypes/kf/kf_lvl_03.tscn"
+			set_current_screen(Enums.SCREENS.KF_LVL_03)
+		Enums.SCREENS.KF_LVL_03:
+			new_level_path = "res://src/scenes/levels/prototypes/gsr/gsr_Confusing.tscn"
+			set_current_screen(Enums.SCREENS.GSR_CONFUSING)
+			MusicManager.play_song("res://assets/music/Godot_Escape_Master1.ogg")
+		Enums.SCREENS.GSR_CONFUSING:
+			new_level_path = "res://src/scenes/levels/prototypes/gsr/gsr_Crossroads.tscn"
+			set_current_screen(Enums.SCREENS.GSR_CROSSROADS)
+		Enums.SCREENS.GSR_CROSSROADS:
+			new_level_path = "res://src/scenes/levels/prototypes/yummy/yjb_lvl_01_IO_Focused.tscn"
+			set_current_screen(Enums.SCREENS.YJB_LVL_01_IO_FOCUSED)
+		Enums.SCREENS.YJB_LVL_01_IO_FOCUSED:
+			new_level_path = "res://src/scenes/levels/prototypes/gsr/gsr_ExpertMode.tscn"
+			set_current_screen(Enums.SCREENS.GSR_EXPERT_MODE)
+		Enums.SCREENS.GSR_EXPERT_MODE:
+			new_level_path = "res://src/scenes/levels/prototypes/yummy/yjb_lvl_02_Titan_Focused.tscn"
+			set_current_screen(Enums.SCREENS.YJB_LVL_02_TITAN_FOCUSED)
+		Enums.SCREENS.YJB_LVL_02_TITAN_FOCUSED:
+			new_level_path = "res://src/scenes/levels/prototypes/gsr/gsr_EasyDeadLevel.tscn"
+			set_current_screen(Enums.SCREENS.GSR_EASY_DEAD_LEVEL)
+		Enums.SCREENS.GSR_EASY_DEAD_LEVEL: 
+			new_level_path = "res://src/scenes/screens/EndCutSceneScreen.tscn"
+			set_current_screen(Enums.SCREENS.END)
+		Enums.SCREENS.END:
+			new_level_path = "res://src/scenes/screens/CreditsScreen.tscn"
+			set_current_screen(Enums.SCREENS.CREDITS)
 	return new_level_path
 
 
@@ -78,6 +139,7 @@ func select_next_screen(target_screen: int) -> String:
 		match target_screen:
 			Enums.SCREENS.MAIN_MENU: 
 				new_level_path = "res://src/scenes/screens/StartScreen.tscn"
+				MusicManager.play_song("res://assets/music/Godot_Slurp_Master1.ogg")
 			Enums.SCREENS.INTRO: 
 				new_level_path = "res://src/scenes/screens/IntroCutSceneScreen.tscn"
 			Enums.SCREENS.CREDITS:
