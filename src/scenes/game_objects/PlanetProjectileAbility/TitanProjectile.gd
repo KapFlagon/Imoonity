8cac91b627ae43e687b0bf6de3c15f2b93839fa1
Jack Grimes <flangojack@hotmail.com>
2020-12-31 12:12:12 +0000

    Fixing bug where a fired projectile for the Titan power will exist forever until secondary trigger is launched to turn into platform. Now times out after 3 seconds.

--- src/scenes/game_objects/PlanetProjectileAbility/TitanProjectile.gd
+++ src/scenes/game_objects/PlanetProjectileAbility/TitanProjectile.gd
@@ -1,9 +1,17 @@
 extends KinematicBody2D
 
 export var projectileSpeed = 5
-var projectileDirection = 0;
 
+
+onready var timer = get_node("ProjectileTimer")
+
+
+var projectileDirection = 0;
 var velocity = Vector2.ZERO
+
+
+func _ready():
+	timer.start()
 
 
 func setprojectileDirection(playerFacing):
@@ -13,3 +21,7 @@ func _physics_process(delta):
 	velocity.x += projectileSpeed * projectileDirection
 	move_and_slide(velocity, Vector2.UP)
 	
+
+
+func _on_ProjectileTimer_timeout():
+	queue_free()
