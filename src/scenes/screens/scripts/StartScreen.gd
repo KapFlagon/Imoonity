53ee00e6bc5a63cc2d77c5301103f9c0f54a39fe
Jack Grimes <flangojack@hotmail.com>
2021-01-08 09:19:41 +0000

    Renaming the 'Play' button to 'New Game', to indicate better that it starts the game right from the beginning.

--- src/scenes/screens/scripts/StartScreen.gd
+++ src/scenes/screens/scripts/StartScreen.gd
@@ -1,18 +1,16 @@
 extends BaseScreen
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	poll_screen_manager(Enums.SCREENS.MAIN_MENU)
 	MusicManager.play_song("res://assets/music/Godot_Slurp_Master1.ogg")
-	pass # Replace with function body.
 
 
 func _on_GithubLinkButton_button_up() -> int:
 	return OS.shell_open("https://github.com/KapFlagon/GHGO2020")
 
 
-func _on_PlayButton_button_up() -> void:
+func _on_NewGameButton_button_up():
 	ScreenManager.next_screen(Enums.SCREENS.INTRO)
 
 
@@ -26,3 +24,4 @@ func _on_HowToPlayButton_button_up() -> void:
 
 func _on_LevelSelectButton_button_up():
 	ScreenManager.next_screen(Enums.SCREENS.LEVELSELECT)
+
